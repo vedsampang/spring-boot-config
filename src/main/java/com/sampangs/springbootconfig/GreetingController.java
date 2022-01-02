c21e9e5fd45e8f05dceb9a2e96efac4fa9643f8c
vedsampang <Oxygen$12>
2022-01-02 11:37:31 -0500

    Init commit

--- src/main/java/com/sampangs/springbootconfig/GreetingController.java
+++ src/main/java/com/sampangs/springbootconfig/GreetingController.java
@@ -1,17 +1,35 @@
 package com.sampangs.springbootconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class GreetingController {
 
-    @Value("${my.greeting}")
+    @Value("${my.greeting: default value}")
     private String greetingMessage;
+
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
+    @Autowired
+    private DbSettings dbSettings;
+
+    //@Value("#{${dbValues}}")
+    //private Map<String, String> dbValues;
+
+
+    public GreetingController(){
+    }
 
     @GetMapping("/greeting")
     public String Greeting(){
-        return greetingMessage;
+        return greetingMessage + listValues + dbSettings.getConnection() + dbSettings.getHost();
     }
 }
